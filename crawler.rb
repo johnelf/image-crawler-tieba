6f02412780b3123beaa2afe14626f44d45c9608d
jinwen <jwzhang@thoughtworks.com>
2014-02-12 10:52:05 +0800

    add gitignore

--- crawler.rb
+++ crawler.rb
@@ -5,7 +5,7 @@ require 'set'
 
 tieba_url = "http://tieba.baidu.com/f?kw=u-know%D4%CA%BA%C6&tp=0"
 page_depth = 50
-article_depth = 1
+article_depth = 2
 
 head_portraits = Set.new
 user_signs = Set.new
@@ -13,6 +13,7 @@ content_images = Set.new
 
 begin 
 	page = Nokogiri::HTML(open(tieba_url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'))
+	puts "打开: " + tieba_url
 
 	tieba_url = nil
 	tieba_pagings = page.css('#frs_list_pager a')
@@ -24,18 +25,13 @@ begin
 	articles = page.css('.threadlist_text.threadlist_title a')
 	articles.each do |article|
 		article_url = article.attributes['href'].value
+		sleep rand(5) + 3
+		puts "打开第一页: " + "http://tieba.baidu.com" + article_url
 		article_content = Nokogiri::HTML(open("http://tieba.baidu.com" + article_url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'))
 		begin 
-			article_url = nil
-			#帖子下一页
-			pagings = article_content.css('.l_pager.pager_theme_3 a')
-			pagings.each do |paging|
-				article_url = paging.attributes['href'].value if paging.children.text == '下一页'
-			end
-
-			break if article_url.nil? or ( article_depth != -1 and article_url.end_with? "pn=" + article_depth.to_s )
-
-			sleep rand(5) + 2
+			
+			sleep rand(5) + 5
+			puts "http://tieba.baidu.com" + article_url
 			article_content = Nokogiri::HTML(open("http://tieba.baidu.com" + article_url, 'User-Agent' => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.107 Safari/537.36'))
 
 			#获取用户头像
@@ -52,8 +48,10 @@ begin
 			signatures = article_content.css('.j_user_sign')
 			signatures.each do |signature|
 				if signature.attributes['data-passive'].nil?
+					puts "***" + signature.attributes['src'].to_s
 					user_signs.add(signature.attributes['src'].to_s)
 				else
+					puts "***" + signature.attributes['data-passive'].value.to_s
 					user_signs.add(signature.attributes['data-passive'].value.to_s)
 				end
 			end
@@ -64,22 +62,40 @@ begin
 				content_images.add(pic.attributes['src'].value.to_s) if ! pic.attributes['src'].value.include? "static.tieba.baidu.com"
 			end
 
+			article_url = nil
+			#帖子下一页
+			pagings = article_content.css('.l_pager.pager_theme_3 a')
+			pagings.each do |paging|
+				article_url = paging.attributes['href'].value if paging.children.text == '下一页'
+			end
+
+			break if article_url.nil? or ( article_depth != -1 and article_url.end_with? "pn=" + article_depth.to_s )
+
 		end while !article_url.nil?
-
 	end
-
+	break
+	puts "===================================================="
+	puts tieba_url + "         " + page_depth.to_s
+	puts tieba_url.end_with? "pn=" + page_depth.to_s
+	puts "===================================================="
 	break if tieba_url.nil? or ( page_depth != -1 and tieba_url.end_with? "pn=" + page_depth.to_s )
 
 end while !tieba_url.nil?
 
-head_portraits.each do |head_portrait|
-	File.open(head_protraits.txt, 'w') { |file| file.write(head_protrait.to_s + "\n") }
+puts "start to write files...."
+puts head_portraits.inspect
+
+working_directory = File.expand_path(File.dirname(File.dirname(__FILE__)))
+head_portraits.each do |head_protrait|
+	File.open(working_directory + "/head_protraits", 'a') { |file| file.write(head_protrait.to_s + "\n") }
 end
 
 user_signs.each do |user_sign|
-	File.open(user_signs.txt, 'w') { |file| file.write(user_sign.to_s + "\n") }
+	File.open(working_directory + "/user_signs", 'a') { |file| file.write(user_sign.to_s + "\n") }
 end
 
 content_images.each do |content_image|
-	File.open(content_images.txt, 'w') { |file| file.write(content_image.to_s + "\n") }
+	File.open(working_directory + "/content_images", 'a') { |file| file.write(content_image.to_s + "\n") }
 end
+
+puts "successful..."
