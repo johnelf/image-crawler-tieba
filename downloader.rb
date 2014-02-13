29502522b4551858bcfed15bdc1c443658ccb568
jinwen <jwzhang@thoughtworks.com>
2014-02-13 13:19:10 +0800

    use wget to download images instead of thread

--- downloader.rb
+++ downloader.rb
@@ -6,8 +6,8 @@ require 'fileutils'
 
 @working_directory = File.expand_path(File.dirname(File.dirname(__FILE__)))
 FileUtils.mkdir_p @working_directory + '/images/user_signs'
-FileUtils.mkdir_p @working_directory + '/images/content'
-FileUtils.mkdir_p @working_directory + '/images/portaits'
+FileUtils.mkdir_p @working_directory + '/images/content_images'
+FileUtils.mkdir_p @working_directory + '/images/head_protraits'
 
 def download(image_url)
 
@@ -23,12 +23,31 @@ def download(image_url)
   end
 end
 
-threads = []
+system 'echo start to downloading...'
+system 'echo images will be saved to #{@working_directory}/images'
 
-File.open(@working_directory + "/user_signs", "r").each do |line|
+system ("wget " "-i " "#{@working_directory}/user_signs " "--directory-prefix=#{@working_directory}/images/user_signs")
+system ("wget " "-i " "#{@working_directory}/head_protraits " "--directory-prefix=#{@working_directory}/images/head_protraits")
+system ("wget " "-i " "#{@working_directory}/content_images " "--directory-prefix=#{@working_directory}/images/content_images")
+
+#threads = []
+
+#File.open(@working_directory + "/user_signs", "r").each do |line|
 	#怕被封IP, 暂时禁用多线程
-	puts "starting to download: #{line}....."
-	threads << Thread.new { download(line) }
-end
+#	puts "starting to download: #{line}....."
+#	threads << Thread.new { download(line) }
+#end
 
-threads.each { |t| t.join }
+#File.open(@working_directory + "/head_protraits", "r").each do |line|
+	#怕被封IP, 暂时禁用多线程
+#	puts "starting to download: #{line}....."
+#	threads << Thread.new { download(line) }
+#end
+
+#File.open(@working_directory + "/content_images", "r").each do |line|
+	#怕被封IP, 暂时禁用多线程
+#	puts "starting to download: #{line}....."
+#	threads << Thread.new { download(line) }
+#end
+
+#threads.each { |t| t.join }
